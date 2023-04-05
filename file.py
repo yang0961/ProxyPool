ac33149ab2eebb3aea101ae69b933e898f6fd96f
Mr.yang <81808194+yang0961@users.noreply.github.com>
2023-04-05 16:27:44 +0800

    Update file.py

--- file.py
+++ file.py
@@ -10,6 +10,7 @@ global_headers = {
     'user-agent': faker.Factory().create().user_agent(),
 }
 
+
 def get_89ip_proxy_ip(number, proxy=None):
     """https://www.89ip.cn/  最多一次提取999个"""
     assert number <= 999, '最多一次获取999个ip'
@@ -23,14 +24,18 @@ def get_89ip_proxy_ip(number, proxy=None):
     params = {'num': number, 'address': '', 'kill_address': '', 'port': '', 'kill_port': '', 'isp': '', }
     proxy_ip_list = []
     proxy_ip_key = ['ip', 'port']
-    if proxy is None:
-        response = get(url=ip_url, headers=headers, params=params).text
-    else:
-        response = get(url=ip_url, headers=headers, params=params, proxies=proxy, timeout=8).text
+    try:
+        if proxy is None:
+            response = get(url=ip_url, headers=headers, params=params).text
+        else:
+            response = get(url=ip_url, headers=headers, params=params, proxies=proxy, timeout=8).text
+    except:
+        print(123)
+        response = get_89ip_proxy_ip(10, proxy={'ip': '94.232.81.95', 'port': '3128'})
     result = re.findall('(?:<!DOCTYPE html>.*?<div style="padding-left:20px;">\s+)?(.*?):(.*?)<br>', response, re.S)
     for ele in result:
         proxy_ip_list.append(dict(zip(proxy_ip_key, ele)))
     return proxy_ip_list
 
 
-print(get_89ip_proxy_ip(10, proxy={'ip': '27.158.127.206', 'port': '8089'}))
+print(get_89ip_proxy_ip(10, proxy={'ip': '94.232.81.95', 'port': '3128'}))
